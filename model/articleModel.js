1e26e9a469a946ffbce9f5a61f884bddbfdb0108
daidaitu1314 <770104768@qq.com>
2017-07-29 15:32:34 +0800

    完成博客案例的分页功能

--- model/articleModel.js
+++ model/articleModel.js
@@ -1,4 +1,5 @@
 var moment = require('moment');
+moment.locale('zh-cn');
 var connection = require('./baseDb.js');
 
 module.exports = {
@@ -23,6 +24,22 @@ module.exports = {
     var sqlStr = 'update articles set ? where id=?';
     connection.query(sqlStr, [article, article.id], (err, results) => {
       if (err) return callback(err);
+      callback(null, results);
+    });
+  },
+  getArticlesByPage(nowPage, pageSize, callback) { // 先获取所有的文章列表，然后在改造成 获取分页
+    // var sqlStr = 'select articles.*, users.nickname from articles left join users on articles.authorId=users.id order by ctime desc';
+    // 偏移量的算法：  (当前页码值 - 1) * 每页显示几条数据
+    var offset = (nowPage - 1) * pageSize;
+
+    var sqlStr = 'select articles.*, users.nickname from articles left join users on articles.authorId=users.id order by ctime desc LIMIT ?, ?; select count(*) AS totalCount from articles;';
+    connection.query(sqlStr, [offset, pageSize], (err, results) => {
+      if (err) return callback(err);
+      console.log(results);
+      // 循环文章列表处理时间
+      results[0].forEach(item => {
+        item.ctime = moment(item.ctime).fromNow();
+      });
       callback(null, results);
     });
   }
--- model/baseDb.js
+++ model/baseDb.js
@@ -1,11 +1,14 @@
 // 向外暴露有个公共的数据库连接实例
 var mysql = require('mysql');
 
+// 注意： 第三方的 mysql 模块。默认没有开启执行多条 Sql 语句的功能，所以，我们执行多条语句时候会报错；
+// 如果要开启多条语句的功能，需要设置一下 配置对象
 var connection = mysql.createConnection({
-  host:'127.0.0.1',
-  user:'root',
-  password:'123456',
-  database:'heima_blog_0728'
+  host: '127.0.0.1',
+  user: 'root',
+  password: '123456',
+  database: 'heima_blog_0728',
+  multipleStatements: true // 开启执行多条Sql语句的功能
 });
 
 module.exports = connection;
